6e29f59929ccedb9b69d38f91acf23190b41e9d8
kritoth <kritoth@gmail.com>
2021-05-06 12:01:26 +0200

    Insert ? operator into observer to avoid NullPointerException

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -16,7 +16,6 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
-import android.app.Application
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -28,7 +27,6 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
-import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
 
 /**
@@ -68,11 +66,19 @@ class SleepTrackerFragment : Fragment() {
         // Do the navigation and then change the state of the event var's state back indicating navigation is done
         sleepViewModel.navigateToSleepQuality.observe(viewLifecycleOwner, Observer {
             nightDone ->
-            nightDone.let {
-                this.findNavController()
-                        .navigate(SleepTrackerFragmentDirections
+            nightDone?.let {
+                // We need to get the navController from this, because button is not ready, and it
+                // just has to be a view. For some reason, this only matters if we hit stop again
+                // after using the back button, not if we hit stop and choose a quality.
+                // Also, in the Navigation Editor, for Quality -> Tracker, check "Inclusive" for
+                // popping the stack to get the correct behavior if we press stop multiple times
+                // followed by back.
+                // Also: https://stackoverflow.com/questions/28929637/difference-and-uses-of-oncreate-oncreateview-and-onactivitycreated-in-fra
+                this.findNavController().navigate(
+                        SleepTrackerFragmentDirections
                                 .actionSleepTrackerFragmentToSleepQualityFragment(nightDone._nightId))
-                //change the state of the event var's state back indicating navigation is done
+                // Reset state to make sure we only navigate once, even if the device
+                // has a configuration change.
                 sleepViewModel.navigationDone()
             }
         })
